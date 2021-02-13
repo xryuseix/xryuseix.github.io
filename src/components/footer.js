8ce1dea7b6af3dee0029eb7d0f61f42b0e2a8c70
xryuseix <ryusei143.shootingstar@gmail.com>
2021-02-13 23:06:10 +0900

    fix: change footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,8 +2,6 @@ import React from "react"
 
 export default () => (
   <footer>
-    © {new Date().getFullYear()}, Built with
-    {` `}
-    <a href="https://www.gatsbyjs.com">Gatsby</a>
+    © {new Date().getFullYear()}, Ryusei Ishikawa All Right Reserved.
   </footer>
 )
