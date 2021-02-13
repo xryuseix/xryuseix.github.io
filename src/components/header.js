fd8cd62632cc632646ed70704f6fd02e6d6c3030
xryuseix <ryusei143.shootingstar@gmail.com>
2021-02-13 19:22:55 +0900

    feat: add header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,47 @@
 import React from "react"
+import { Link } from "gatsby"
+import '../components/header.css'
 
 export default () => (
   <header>
-    <h1>サンプルサイトのヘッダー!</h1>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top" style={{height: "4em"}}>
+      <div class="container">
+        <Link href="/" class="navbar-brand">Home</Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#base-navbars"
+          aria-controls="base-navbars"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="base-navbars">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <Link class="nav-link" href="/">News</Link>
+            </li>
+            <li class="nav-item dropdown">
+              <Link href="/" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">Applications</Link>
+              <div class="dropdown-menu mt-0" aria-labelledby="navbarDropdown">
+                <Link href="https://github.com/xryuseix/SA-Plag" target="_blank" rel="noopener noreferrer" tabindex="0" role="menuitem" class="dropdown-item">GitHub</Link>
+                <Link href="https://twitter.com/ryusei_ishika" target="_blank" rel="noopener noreferrer" tabindex="0" role="menuitem" class="dropdown-item">Contact</Link>
+              </div>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="/">Career</Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="/">Account</Link>
+            </li>
+            <li class="nav-item">
+              <Link class="nav-link" href="https://github.com/xryuseix" target="_blank">Repository</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
 )
