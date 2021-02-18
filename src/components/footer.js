e3d194e13e438e0619dbe56410fafc5c80e7f65b
xryuseix <ryusei143.shootingstar@gmail.com>
2021-02-19 08:56:37 +0900

    update readme

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,8 @@
 import React from 'react'
 
-export default () => <footer>© {new Date().getFullYear()}, Ryusei Ishikawa All Right Reserved.</footer>
+export default () => (
+    <footer>
+        <img src="http://www.rays-counter.com/d479_f6_001/602efc0037e78/" alt="アクセスカウンター" border="0" style={{ display: 'none' }} />
+        © {new Date().getFullYear()}, Ryusei Ishikawa All Right Reserved.
+    </footer>
+)
