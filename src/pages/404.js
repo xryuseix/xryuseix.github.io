5c322a84ad8fa0a48c7efed4d4c3dfe4fbfb1beb
xryuseix <ryusei143.shootingstar@gmail.com>
2021-02-19 22:09:34 +0900

    fix: 404ページのフォント崩れを修正

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,15 +3,17 @@ import { graphql } from 'gatsby'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Meta from '../components/meta'
 
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
+const NotFoundPage = ({ location }) => {
+  const siteTitle = "404: Not Found"
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
       <h1>404: Not Found</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <Meta title="404" />
     </Layout>
   )
 }
