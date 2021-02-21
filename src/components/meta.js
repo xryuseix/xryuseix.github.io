3c4a3fac5b407fac0f6b5f4d2fa5bd2fcd307879
xryuseix <ryusei143.shootingstar@gmail.com>
2021-02-21 15:26:58 +0900

    fix SEO対策のため，metaタグのdescription追加

--- src/components/meta.js
+++ src/components/meta.js
@@ -3,11 +3,12 @@ import { Helmet } from 'react-helmet'
 
 export default (props) => {
   const title = `${props.title}`
+  const description = 'xryuseixのWebページです．ここからはTwitterのbioから引用 -> Rits Security Network(入学: &#39;19) / SecHack365&#39;20(研究駆動) / seccamp&#39;19 / RiPPro(&#39;20団体長) / RiST(&#39;20副団体長) / AtCoder(cyan) / CodeForces(blue) / paiza, Inc. 学習事業部アルバイト / 競技プログラミングやセキュリティの分野を勉強しています．'
   return (
     <Helmet>
       <html lang="ja" />
       <title>{title}</title>
-      <meta name="description"/>
+      <meta name="Description" content={description} />
       <meta charset="UTF-8" />
     </Helmet>
   )
