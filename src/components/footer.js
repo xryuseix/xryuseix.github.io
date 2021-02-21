92879cef06ffc5b691b4a610bdcf5ab56d4fcfcc
xryuseix <ryusei143.shootingstar@gmail.com>
2021-02-21 15:04:18 +0900

    fix: アクセスカウンターのカウントリセット

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => (
     <footer>
-        <img src="https://www.f-counter.net/j/45/1613887003/" alt="アクセスカウンター" border="0" style={{ display: 'none' }} />
+        <img src="https://www.f-counter.net/j/45/1613887388/" alt="アクセスカウンター" border="0" style={{ display: 'none' }} />
         © {new Date().getFullYear()}, Ryusei Ishikawa All Right Reserved.
     </footer>
 )
