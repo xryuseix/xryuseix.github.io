3e50012c4dcc06c3ce6e8bec4f2b0e2bd0d3ea38
xryuseix <ryusei143.shootingstar@gmail.com>
2021-02-21 14:59:00 +0900

    fix: アクセスカウンターがHTTP通信だったので，HTTPS通信ができるサイトに変更

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default () => (
     <footer>
-        <img src="http://www.rays-counter.com/d479_f6_001/602efc0037e78/" alt="アクセスカウンター" border="0" style={{ display: 'none' }} />
+        <img src="https://www.f-counter.net/j/45/1613887003/" alt="アクセスカウンター" border="0" style={{ display: 'none' }} />
         © {new Date().getFullYear()}, Ryusei Ishikawa All Right Reserved.
     </footer>
 )
