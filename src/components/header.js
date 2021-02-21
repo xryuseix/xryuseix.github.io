de357684c51bf075cc787df5c70c9b162ca0f985
xryuseix <ryusei143.shootingstar@gmail.com>
2021-02-21 12:13:25 +0900

    add link icon

--- src/components/header.js
+++ src/components/header.js
@@ -38,6 +38,20 @@ export default () => (
             <Nav.Item as="li">
               <Link class="nav-link" href="https://github.com/xryuseix/xryuseix.github.io" target="_blank">
                 Repository
+                <svg
+                  width="1em"
+                  height="1em"
+                  viewBox="0 1 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
+                  <polyline points="15 3 21 3 21 9" />
+                  <line x1="10" y1="14" x2="21" y2="3" />
+                </svg>
               </Link>
             </Nav.Item>
           </Nav>
