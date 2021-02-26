4be7a54bd55e434f8bb8bb8c2bcf7eadd2f7de54
xryuseix <ryusei143.shootingstar@gmail.com>
2021-02-26 17:19:15 +0900

    feat: add toggle button

---
+++ src/components/flagSubmit.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class ToggleClass extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isToggleOn: true }
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState((state) => ({
+      isToggleOn: !state.isToggleOn
+    }))
+  }
+
+  render() {
+    return <button onClick={this.handleClick}>{this.state.isToggleOn ? 'ON' : 'OFF'}</button>
+  }
+}
+
+
+export default ToggleClass
--- src/pages/ctf.js
+++ src/pages/ctf.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Helmet from 'react-helmet'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Meta from '../components/meta'
-
+import Toggle from '../components/flagSubmit'
 import './ctf.css'
 
 const SiteIndex = ({ location }) => {
@@ -14,61 +13,21 @@ const SiteIndex = ({ location }) => {
       <Meta title="CTF" />
       <h1>CTF</h1>
       <p>CTF問題をここにおきます．基本的に簡単です．</p>
-      <p>このページはめんどくさくなってまだ出来てないです．問題は一問だけ置いてあります．</p>
+      <h3>このページのソースはみないでね，フラグがそのまま載ってます．</h3>
       <div class="hidden_box">
-        <label htmlFor="label1">[Web](100) 問題テスト</label>
+        <label htmlFor="label1">[Welcome](100) 問題テスト</label>
         <input type="checkbox" id="label1" />
         <div class="hidden_show">
-          <p>このサイトの内容が多すぎて困ってます．検索ページを付けようか迷ってます...</p>
-          <form>
-            <input type="text" id="textForm" />
-            <button id="button" onClick={flagSubmit()}>
-              Submit!
-            </button>
-          </form>
-          <input type="text" id="resultForm" />
+          <p>フラグ形式のテスト</p>
+          <Toggle />
         </div>
       </div>
-      <div class="hidden_box">
-        <label htmlFor="label2">テスト２</label>
-        <input type="checkbox" id="label2" />
-        <div class="hidden_show">
-          <p>テスト</p>
-        </div>
-      </div>
-      <Helmet>
-        <script type="text/javascript">{`
-        function flagSubmit(){
-            // ボタンの要素を取得
-            var button = document.getElementById("button");
-
-            // ボタンをクリックした時の処理
-            button.addEventListener("click", function(e) {
-                e.preventDefault();
-                // 入力フォームの値を取得
-                var textForm = document.getElementById("textForm").value;
-                // 取得した値を別の入力フォームに表示
-                var resultForm = document.getElementById("resultForm");
-                resultForm.value = textForm;
-            });
-        }`}</script>
-      </Helmet>
     </Layout>
   )
 }
-function flagSubmit() {
-    // ボタンの要素を取得
-    var button = document.getElementById("button");
 
-    // ボタンをクリックした時の処理
-    button.addEventListener("click", function(e) {
-        e.preventDefault();
-        // 入力フォームの値を取得
-        var textForm = document.getElementById("textForm").value;
-        // 取得した値を別の入力フォームに表示
-        var resultForm = document.getElementById("resultForm");
-        resultForm.value = textForm;
-    });
-}
+
+
 
 export default SiteIndex
+/* <p>このサイトの内容が多すぎて困ってます．検索ページを付けようか迷ってます...</p> */
