b4e10883cde5e8b59f0ebfc089a41dfe9353198a
xryuseix <ryusei143.shootingstar@gmail.com>
2021-02-26 19:27:46 +0900

    feat: add problem

--- src/pages/ctf.js
+++ src/pages/ctf.js
@@ -15,11 +15,27 @@ const SiteIndex = ({ location }) => {
       <p>CTF問題をここにおきます．基本的に簡単です．</p>
       <h3>このページのソースはみないでね，フラグがそのまま載ってます．</h3>
       <div class="hidden_box">
-        <label htmlFor="label1">[Welcome](100) 問題テスト</label>
+        <label htmlFor="label1">[Welcome](100) Welcome Problem</label>
         <input type="checkbox" id="label1" />
         <div class="hidden_show">
-          <p>フラグ形式のテスト</p>
-          <FlagSubmit flag="xryuseixCTF{flag}" />
+          <p>
+            This Problems Answer is <code>xryuseixCTF&#123;flag&#125;</code>
+          </p>
+          <div class="submit-column input">
+            <input type="text" id="textForm1" />
+          </div>
+          <FlagSubmit flag="xryuseixCTF{flag}" id="textForm1" />
+        </div>
+      </div>
+      <div class="hidden_box">
+        <label htmlFor="label2">[Web](100) Wandering</label>
+        <input type="checkbox" id="label2" />
+        <div class="hidden_show">
+          <p>このサイトの内容が多すぎて困ってます．検索ページを付けようか迷ってます...</p>
+          <div class="submit-column input">
+            <input type="text" id="textForm2" />
+          </div>
+          <FlagSubmit flag="xryuseixCTF{XSS_51y0ut051t3t3_ku54}" id="textForm2" />
         </div>
       </div>
     </Layout>
@@ -27,4 +43,3 @@ const SiteIndex = ({ location }) => {
 }
 
 export default SiteIndex
-/* <p>このサイトの内容が多すぎて困ってます．検索ページを付けようか迷ってます...</p> */
