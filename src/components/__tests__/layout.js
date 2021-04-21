84bc927f992290402ce41ab94ef11fed95e1deb4
xryuseix <ryusei143.shootingstar@gmail.com>
2021-04-22 02:15:43 +0900

    test: add layout test

---
+++ src/components/__tests__/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Layout from '../layout'
+describe('Layout', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<Layout location="/" />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
