db61c5c279dc65fc90ee3a41eed728c60fe22d15
xryuseix <ryusei143.shootingstar@gmail.com>
2021-04-22 03:36:09 +0900

    refactor: Appのコンテンツを分離した

--- src/pages/apps.js
+++ src/pages/apps.js
@@ -5,10 +5,93 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Meta from '../components/meta'
 import { StaticImage } from 'gatsby-plugin-image'
-import saplagDemo from '../images/saplag/sa-plag_demo.gif'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 import './apps.css'
+
+const AppData = [
+  {
+    appId: 'sa_plag',
+    appLink: '/apps/sa-plag',
+    imageSrc: '../images/saplag/sa-plag_demo.png',
+    imageAlt: 'SA-Plag Demo',
+    appTitle: 'SA-Plag',
+    appDesc: 'ソースコードの盗作を判定するWeb APIです．<br />AIが競技プログラミングのソースコードを学習しました．',
+    webPageLink: '/apps/sa-plag',
+    githubLink: 'https://github.com/xryuseix/SA-Plag'
+  },
+  {
+    appId: 'xryuseix_judge',
+    appLink: '/apps/contest_judge',
+    imageSrc: '../images/xryuseix_judge.png',
+    imageAlt: 'xryuseix judge',
+    appTitle: 'xryuseix judge',
+    appDesc: '簡易的なクイズの成績判定システムです． 立命館大学プロジェクト連合合同イベントなどで使用しました．',
+    webPageLink: '/apps/contest_judge',
+    githubLink: ''
+  }
+]
+
+/*
+ アプリケーション管理
+ appId ... 固有ID
+ appPageLink ... アプリケーションのリンク
+ imageSrc ... 画像のリンク
+ imageAlt ... 画像
+ appTitle ... タイトル
+ appDesc ... 説明
+ webPageLink ... Webページのリンク
+ githubLink ... GitHubのリンク
+*/
+
+const Apps = ({ appId, appLink, imageSrc, imageAlt, appTitle, appDesc, webPageLink, githubLink }) => (
+  <div id={appId}>
+    <table style={{ margin: '30px 0px' }}>
+      <tr>
+        <td style={{ width: '50%' }}>
+          <Link to={appLink}>
+            <img src={imageSrc} alt={imageAlt} style={{ width: '100%', height: 'auto' }} />
+          </Link>
+        </td>{' '}
+        <td valign="top">
+          <table style={{ margin: '0px 0px 0px 10px' }}>
+            <tr align="center">
+              <th colspan="2" style={{ 'font-size': '2em' }}>
+                {appTitle}
+              </th>
+            </tr>
+            <tr>
+              <td colspan="2">{appDesc}</td>
+            </tr>
+            <tr align="center">
+              <td style={{ width: '50%' }}>
+                <Link to={webPageLink}>
+                  <StaticImage
+                    src="../../static/favicons/icon-32x32.png"
+                    alt="favicon"
+                    style={{ width: '1em', margin: '0px 0.3em 0px 0px' }}
+                  />
+                  Webサイト
+                </Link>
+              </td>{' '}
+              <td style={{ width: '50%' }}>
+                <a href={githubLink} target="_blank" rel="noopener noreferrer">
+                  <StaticImage
+                    src="../../static/icons/GitHub.png"
+                    alt="GitHub logo"
+                    style={{ width: '1em', margin: '0px 0.3em 0px 0px' }}
+                  />
+                  GitHub
+                </a>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+    </table>
+    <hr />
+  </div>
+)
 
 const SiteIndex = ({ location }) => {
   return (
@@ -33,100 +116,9 @@ const SiteIndex = ({ location }) => {
         </ul>
       </details>
 
-      <br />
-
-      <div id="sa_plag">
-        <table style={{ margin: '30px 0px' }}>
-          <tr>
-            <td style={{ width: '50%' }}>
-              <Link to="/apps/sa-plag">
-                <img src={saplagDemo} alt="SA-Plag Demo" style={{ width: '100%', height: 'auto' }} />
-              </Link>
-            </td>{' '}
-            <td valign="top">
-              <table style={{ margin: '0px 0px 0px 10px' }}>
-                <tr align="center">
-                  <th colspan="2" style={{ 'font-size': '2em' }}>
-                    SA-Plag
-                  </th>
-                </tr>
-                <tr>
-                  <td colspan="2">
-                    ソースコードの盗作を判定するWeb APIです．
-                    <br />
-                    AIが競技プログラミングのソースコードを学習しました．
-                  </td>
-                </tr>
-                <tr align="center">
-                  <td style={{ width: '50%' }}>
-                    <Link to="/apps/sa-plag">
-                      <StaticImage
-                        src="../../static/favicons/icon-32x32.png"
-                        alt="favicon"
-                        style={{ width: '1em', margin: '0px 0.3em 0px 0px' }}
-                      />
-                      Webサイト
-                    </Link>
-                  </td>{' '}
-                  <td style={{ width: '50%' }}>
-                    <a href="https://github.com/xryuseix/SA-Plag" target="_blank" rel="noopener noreferrer">
-                      <StaticImage
-                        src="../../static/icons/GitHub.png"
-                        alt="GitHub logo"
-                        style={{ width: '1em', margin: '0px 0.3em 0px 0px' }}
-                      />
-                      GitHub
-                    </a>
-                  </td>
-                </tr>
-              </table>
-            </td>
-          </tr>
-        </table>
-      </div>
-      <hr />
-      <div id="xryuseix_judge">
-        <table style={{ margin: '30px 0px' }}>
-          <tr>
-            <td style={{ width: '50%' }}>
-              <Link to="/apps/contest_judge">
-                <StaticImage
-                  src="../images/xryuseix_judge.png"
-                  alt="xryuseix judge"
-                  style={{ width: '100%', height: 'auto' }}
-                />
-              </Link>
-            </td>{' '}
-            <td valign="top">
-              <table style={{ margin: '0px 0px 0px 10px' }}>
-                <tr align="center">
-                  <th colspan="2" style={{ 'font-size': '2em' }}>
-                    xryuseix judge
-                  </th>
-                </tr>
-                <tr>
-                  <td colspan="2">
-                    簡易的なクイズの成績判定システムです． 立命館大学プロジェクト連合合同イベントなどで使用しました．
-                  </td>
-                </tr>
-                <tr align="center">
-                  <td style={{ width: '50%' }}>
-                    <Link to="/apps/contest_judge">
-                      <StaticImage
-                        src="../../static/favicons/icon-32x32.png"
-                        alt="favicon"
-                        style={{ width: '1em', margin: '0px 0.3em 0px 0px' }}
-                      />
-                      Webサイト
-                    </Link>
-                  </td>{' '}
-                  <td style={{ width: '50%' }}></td>
-                </tr>
-              </table>
-            </td>
-          </tr>
-        </table>
-      </div>
+      {AppData.map((data) => (
+        <Apps {...data} />
+      ))}
     </Layout>
   )
 }
