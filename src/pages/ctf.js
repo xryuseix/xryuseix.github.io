23af05308950d72a00c9b59c2899d11f0457fb44
xryuseix <ryusei143.shootingstar@gmail.com>
2021-05-23 01:15:48 +0900

    refactor: idとlabelを削除

--- src/pages/ctf.js
+++ src/pages/ctf.js
@@ -9,33 +9,29 @@ import './ctf.css'
 const ProblemsData = [
   {
     title: '[Welcome](100) Welcome Problem',
-    label: 'label1',
     statement: (
       <>
         This Problems Answer is <code>xryuseixCTF&#123;flag&#125;</code>
       </>
     ),
     flag: 'xryuseixCTF{flag}',
-    id: 'textForm1'
   },
   {
     title: '[Web](100) Wandering',
-    label: 'label2',
     statement: "このサイトの内容が多すぎて困ってます．検索ページを付けようか迷ってます...",
     flag: 'xryuseixCTF{XSS_51y0ut051t3t3_ku54}',
-    id: 'textForm2'
   }
 ]
 
 /* 
  問題を全て出力
  title ... 問題タイトル
- label ... 問題ラベル
  statement ... 問題文
  flag ... 答え
- id ... 問題ID
 */
-const Problems = ({ title, label, statement, flag, id }) => {
+const Problems = ({ title,  statement, flag }) => {
+  const label = Math.random().toString(36).slice(-12)
+  const id = Math.random().toString(36).slice(-12)
   return (
     <div class="hidden_box">
       <label htmlFor={label}>{title}</label>
