f12f6fade9a8ba40879b44903137d5dc710b5591
xryuseix <xryuseix@users.noreply.github.com>
2021-06-23 15:47:57 +0000

    deploy: 58efa5150f81aaed5fc392124460ab0274f67c73

---
+++ component---src-pages-search-js-d8dfbf5dcbdbe31a1bdb.js
@@ -0,0 +1,2 @@
+(self.webpackChunk=self.webpackChunk||[]).push([[996],{5068:function(e,t,a){"use strict";a.r(t);var l=a(7294),n=a(5414),r=a(8931),c=a(5909);t.default=function(e){var t=e.location;return l.createElement(r.Z,{location:t},l.createElement(c.Z,{title:"XSS Page"}),l.createElement(n.Z,null,l.createElement("script",{type:"text/javascript"},'alert("クロスサイトは(多分)できないよ♡");')),l.createElement("h2",null,"xryuseixCTF{XSS_51y0ut051t3t3_ku54}"))}}}]);
+//# sourceMappingURL=component---src-pages-search-js-d8dfbf5dcbdbe31a1bdb.js.map
