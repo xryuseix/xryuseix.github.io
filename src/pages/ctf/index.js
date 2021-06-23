c82f5e320d449688c669c318534fd52d0d2af527
xryuseix <ryusei143.shootingstar@gmail.com>
2021-06-23 21:05:16 +0900

    feat: add problem misc-Test

--- src/pages/ctf/index.js
+++ src/pages/ctf/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-import Layout from '../components/layout'
-import SEO from '../components/seo'
-import Meta from '../components/meta'
-import FlagSubmit from '../components/flagSubmit'
+import Layout from '../../components/layout'
+import SEO from '../../components/seo'
+import Meta from '../../components/meta'
+import FlagSubmit from '../../components/flagSubmit'
 import './ctf.css'
+import misc_Test from './content/enc_pdf.pdf'
 
 const ProblemsData = [
   {
@@ -14,12 +15,14 @@ const ProblemsData = [
         This Problems Answer is <code>xryuseixCTF&#123;flag&#125;</code>
       </>
     ),
-    flag: 'xryuseixCTF{flag}'
+    flag: 'xryuseixCTF{flag}',
+    answer: 'xryuseixCTF{flag}'
   },
   {
     title: '[Web](100) Wandering',
     statement: 'このサイトの内容が多すぎて困ってます．検索ページを付けようか迷ってます...',
-    flag: 'xryuseixCTF{XSS_51y0ut051t3t3_ku54}'
+    flag: 'xryuseixCTF{XSS_51y0ut051t3t3_ku54}',
+    answer: 'https://xryuseix.github.io/search/'
   },
   {
     title: '[misc](200) Time measurement',
@@ -32,7 +35,27 @@ const ProblemsData = [
         </a>
       </>
     ),
-    flag: 'xryuseixCTF{flag}'
+    flag: 'xryuseixCTF{flag}',
+    answer: 'ReDoS攻撃が成立します'
+  },
+  {
+    title: '[misc](200) Test',
+    statement: (
+      <>
+        オンラインテストに向けてテスト用紙が配られました．あなたは試験開始前に問題を閲覧し，解きたいと思っています．頑張って解いてみてください！
+        <br />
+        <a href={misc_Test} download>
+          [PDFファイル]
+        </a>
+      </>
+    ),
+    flag: 'xryuseixCTF{onaka_suita}',
+    answer: (
+      <>
+        <code>qpdf --decrypt --password="password" problem.pdf out2.pdf</code> の後にWordで開く，または
+        <code>qpdf --decrypt --password="risec" problem.pdf out2.pdf</code> の後に該当部分をコピーする．
+      </>
+    )
   }
 ]
 
@@ -42,7 +65,7 @@ const ProblemsData = [
  statement ... 問題文
  flag ... 答え
 */
-const Problems = ({ title, statement, flag }) => {
+const Problems = ({ title, statement, flag, answer }) => {
   const label = Math.random().toString(36).slice(-12)
   const id = Math.random().toString(36).slice(-12)
   return (
@@ -55,6 +78,10 @@ const Problems = ({ title, statement, flag }) => {
           <input type="text" id={id} />
         </div>
         <FlagSubmit flag={flag} id={id} />
+        <details style={{ display: 'none' }}>
+          <summary>答え</summary>
+          {answer}
+        </details>
       </div>
     </div>
   )
