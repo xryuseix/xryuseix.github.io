ad206c5533e56756917662f13b62f2610c01f973
xryuseix <ryusei143.shootingstar@gmail.com>
2021-06-25 03:02:53 +0900

    feat: キーボードでスライド操作機能追加

--- src/pages/slides.js
+++ src/pages/slides.js
@@ -3,7 +3,7 @@ import { pdfjs, Document, Page } from 'react-pdf'
 
 import Layout from '../components/layout'
 import Meta from '../components/meta'
-import SEO from '../components/seo'
+import Seo from '../components/seo'
 
 import samplePDF from './sample.pdf'
 
@@ -12,29 +12,50 @@ const Slides = [
     title: 'Python2.xのinput関数にRCE脆弱性がある話',
     description: 'これはサンプルの説明です',
     content: samplePDF,
-    page: 15,
+    page: 16,
     keyword: 'security'
   }
 ]
 
 class Counter extends React.Component {
-  constructor(props, page) {
+  constructor(props) {
     super(props)
     this.state = {
       page: 1,
-      maxpage: 16
+      maxpage: props.maxpage,
+      lastkey: -1
     }
+  }
+
+  handleKeyDown(e) {
+    if (e.keyCode === 39 || e.keyCode === 40) {
+      // 右, 下
+      this.setState({ page: Math.min(this.state.page + 1, this.state.maxpage) })
+    } else if (e.keyCode === 37 || e.keyCode === 38) {
+      // 左, 上
+      this.setState({ page: Math.max(this.state.page - 1, 1) })
+    } else if (e.keyCode >= 49 || e.keyCode <= 57) {
+      // 数字
+      this.setState({ page: Math.min(e.keyCode - 48, this.state.maxpage) })
+    }
+    this.setState({ lastkey: e.keyCode })
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown.bind(this))
   }
 
   render() {
     return (
       <div>
         <Page pageNumber={this.state.page} />
-        <button onClick={() => this.setState({ page: Math.max(this.state.page - 1, 1) })}>前のスライド</button>
-        <button onClick={() => this.setState({ page: Math.min(this.state.page + 1, this.state.maxpage) })}>
-          次のスライド
-        </button>
-        <div style={{ 'text-align': 'center' }}>スライド{this.state.page}</div>
+        <div style={{ 'text-align': 'right' }}>スライド{this.state.maxpage}</div>
+        <div>
+          <button onClick={() => this.setState({ page: Math.max(this.state.page - 1, 1) })}>前のスライド</button>
+          <button onClick={() => this.setState({ page: Math.min(this.state.page + 1, this.state.maxpage) })}>
+            次のスライド
+          </button>
+        </div>
       </div>
     )
   }
@@ -42,13 +63,14 @@ class Counter extends React.Component {
 
 const SlideSiteIndex = ({ location }) => {
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
   return (
     <Layout location={location}>
-      <SEO title="My slides" description="スライド一覧" />
+      <Seo title="My slides" description="スライド一覧" />
       <Meta title="Slides" />
       {Slides.map((data) => (
         <Document file={data.content}>
-          <Counter page={data.page} />
+          <Counter maxpage={data.page} />
         </Document>
       ))}
     </Layout>
