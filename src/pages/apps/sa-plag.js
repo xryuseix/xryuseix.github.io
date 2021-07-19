c4c3c9cc8204adc0f215709b67066bdf4fc99a35
xryuseix <ryusei143.shootingstar@gmail.com>
2021-07-20 02:13:07 +0900

    refactor: style要素の削除と画像の読み込み先を変更

--- src/pages/apps/sa-plag.js
+++ src/pages/apps/sa-plag.js
@@ -3,10 +3,11 @@ import React from 'react'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 import Meta from '../../components/meta'
-// import { StaticImage } from 'gatsby-plugin-image'
+import { StaticImage } from 'gatsby-plugin-image'
+
 import './sa-plag.css'
 
-// import saplagDemo from '../../images/saplag/sa-plag_demo.gif'
+import saplagDemo from './images/saplag/sa-plag_demo.gif'
 
 const SiteIndex = ({ location }) => {
   return (
@@ -17,13 +18,13 @@ const SiteIndex = ({ location }) => {
       />
       <Meta title="SA-Plag" />
 
-      {/* <table style={{ 'text-align': 'center' }}>
+      <table className="sa-plag_center">
         <tr>
-          <th style={{ width: '20%', height: 'auto' }}>
-            <StaticImage src="../../images/saplag/sa-plag_logo.png" alt="SA-Plag logo" />
+          <th className="sa-plag_title_logo">
+            <StaticImage src="./images/saplag/sa-plag_logo.png" alt="SA-Plag logo" />
           </th>
-          <th style={{ width: '70%', height: 'auto', 'text-align': 'left' }}>
-            <div style={{ 'font-size': '4em' }}>SA-Plag</div>
+          <th className="sa-plag_title">
+            <div className="sa-plag_title_font">SA-Plag</div>
           </th>
         </tr>
       </table>
@@ -32,26 +33,22 @@ const SiteIndex = ({ location }) => {
         <p>ソースコードの盗作を判定するWeb APIです．</p>
         <p>AIが競技プログラミングのソースコードを学習しました．</p>
       </div>
-      <div style={{ 'text-align': 'center' }}>
+      <div className="sa-plag_center">
         <a href="https://sa-plag.herokuapp.com" target="_blank" rel="noopener noreferrer">
-          <img src={saplagDemo} alt="SA-Plag Demo" style={{ width: '100%', height: 'auto' }} />
+          <img src={saplagDemo} alt="SA-Plag Demo" className="sa-plag_demo" />
         </a>
         <h3>使用するには画像をクリックしてください</h3>
       </div>
       <br />
       <hr />
       <br />
-      <div style={{ 'text-align': 'center' }}>
+      <div className="sa-plag_center">
         この成果物はSecHack365内で作成されました．
         <br />
         <a href="https://sechack365.nict.go.jp/" target="_blank" rel="noopener noreferrer">
-          <StaticImage
-            src="../../images/saplag/SecHack365.png"
-            alt="SecHack365 logo"
-            style={{ width: '40%', height: 'auto' }}
-          />
+          <StaticImage src="./images/saplag/SecHack365.png" alt="SecHack365 logo" className="sa-plag_sh" />
         </a>
-      </div> */}
+      </div>
     </Layout>
   )
 }
