1c7d8ace615b2e96d4ecc04ee01999d1b91c1c41
xryuseix <xryuseix@users.noreply.github.com>
2021-09-03 09:21:48 +0000

    deploy: 0f67f566fb17775eb51c0a538e15410a1343e08e

--- 252f366e-dcf2d7209dd966fb883c.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk=self.webpackChunk||[]).push([[228],{2775:function(t,n,r){"use strict";r.d(n,{FU5:function(){return u},Yc6:function(){return c}});var a=r(1046);function u(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"}}]})(t)}function c(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"}}]})(t)}}}]);
-//# sourceMappingURL=252f366e-dcf2d7209dd966fb883c.js.map
--- component---src-pages-search-js-d62993a45277861dab6c.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk=self.webpackChunk||[]).push([[996],{5068:function(e,t,a){"use strict";a.r(t);var l=a(7294),n=a(5414),r=a(5033),c=a(5909);t.default=function(e){var t=e.location;return l.createElement(r.Z,{location:t},l.createElement(c.Z,{title:"XSS Page"}),l.createElement(n.Z,null,l.createElement("script",{type:"text/javascript"},'alert("クロスサイトは(多分)できないよ♡");')),l.createElement("h2",null,"xryuseixCTF{XSS_51y0ut051t3t3_ku54}"))}}}]);
-//# sourceMappingURL=component---src-pages-search-js-d62993a45277861dab6c.js.map
