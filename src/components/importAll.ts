f1abdc9a760f3ac21bb1340a03b1e6b8e2f49672
xryuseix <ryusei143.shootingstar@gmail.com>
2021-12-06 03:05:46 +0900

    refactor: importallのTS化

---
+++ src/components/importAll.ts
@@ -0,0 +1,16 @@
+/**
+ * コンテンツを一括importして動的に呼び出せるようにする
+ * @param reqContent require.contextの返り値
+ */
+
+interface Items {
+  [k: string]: string
+}
+
+const importAll: Function = (reqContent: __WebpackModuleApi.RequireContext): Items => {
+  let items: Items = {}
+  reqContent.keys().map((item: string) => (items[item.replace('./', '')] = reqContent(item) as string))
+  return items
+}
+
+export default importAll
