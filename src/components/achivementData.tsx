6065870385435ac5f1531a84a96ecb78ee2b7934
xryuseix <ryusei143.shootingstar@gmail.com>
2021-12-06 03:28:37 +0900

    refactor: achivementのデータを別コンポーネントに分離

---
+++ src/components/achivementData.tsx
@@ -0,0 +1,204 @@
+import React from 'react'
+import { MdOpenInNew } from 'react-icons/md'
+
+import IPAVulnList from '../utils/ipaVulnerability/vulnList'
+
+export const SchoolData = [
+  {
+    year: '2013-2019',
+    detail: '神奈川県立平塚中等教育学校',
+    url: 'https://www.pen-kanagawa.ed.jp/hiratsuka-chuto-ss/'
+  },
+  {
+    year: '2019-',
+    detail: (
+      <>
+        立命館大学 情報理工学部 セキュリティ・ネットワークコース
+        <details>
+          <summary>詳細</summary>
+          <table className="achievement_min-table">
+            <tr>
+              <td className="achievement_min-table-title">入試方式</td>{' '}
+              <td className="achievement_min-table-content">
+                AO入試
+                <details className="achievement_details">
+                  <summary>提出した自作ソフトウェア</summary>
+                  <ul>
+                    <li>暗号化ソフトウェア</li>
+                    <li>
+                      シーザー暗号，ADFGVX暗号，ワンタイムパッド，自作暗号である
+                      <a href="https://github.com/xryuseix/cubing_cipher" target="_blank" rel="noopener noreferrer">
+                        cubing暗号
+                        <MdOpenInNew />
+                      </a>
+                      を暗号化・復号できるソフトウェア(現在の仕様と大幅に異なる)
+                    </li>
+                  </ul>
+                </details>
+              </td>
+            </tr>
+            <tr>
+              <td className="achievement_min-table-title">サークル</td>{' '}
+              <td className="achievement_min-table-content">
+                <ul>
+                  <li>情報理工学部プロジェクト団体 RiPPro 2020年度団体長</li>
+                  <li>情報理工学部プロジェクト団体 RiST 2020年度副団体長</li>
+                </ul>
+              </td>
+            </tr>
+          </table>
+        </details>
+      </>
+    ),
+    url: 'http://www.ritsumei.ac.jp/'
+  }
+]
+
+const WorkData = [
+  {
+    year: '2018',
+    detail: '総務省 地域におけるIoTの学び推進事業地域実証事業 メンター',
+    url: 'https://www.soumu.go.jp/main_content/000605601.pdf'
+  },
+  {
+    year: '2020-',
+    detail: 'paiza株式会社 学習事業部 学生スタッフ',
+    url: 'https://www.paiza.co.jp/'
+  },
+  {
+    year: '2021',
+    detail: 'デロイト トーマツ サイバー合同会社 3daysインターンシップ',
+    url: 'https://www.onecareer.jp/events/23881'
+  },
+  {
+    year: '2021',
+    detail: 'ＮＴＴ東日本 夏季DS/DX/セキュリティ 5daysインターンシップ',
+    url: 'https://www.ntt-east.co.jp/recruit/internship-tech/'
+  }
+]
+
+const EventData = [
+  {
+    year: '2019',
+    detail: 'セキュリティ・キャンプ全国大会2019 集中開発コース 暗号化通信ゼミ',
+    url: 'https://www.ipa.go.jp/jinzai/camp/2019/zenkoku2019_vote.html'
+  },
+  {
+    year: '2020-2021',
+    detail: '若手セキュリティイノベーター育成プログラム SecHack365 研究駆動コース',
+    url: 'https://sechack365.nict.go.jp/'
+  },
+  {
+    year: 2020,
+    detail: (
+      <>
+        AVTOKYO 2020 Talks
+        <details className="achievement_details">
+          <summary>発表テーマ</summary>
+          <ul>
+            <li>
+              [jp] ビデオチャットの映像から部屋が盗み見られる！？
+              バーチャル背景適用済み映像から部屋の画像を復元する手法とその自動化 (石川 琉聖, 辻 知希)
+            </li>
+          </ul>
+        </details>
+      </>
+    ),
+    url: 'https://www.avtokyo.org/2020'
+  },
+  {
+    year: 2021,
+    detail: (
+      <>
+        情報通信システムセキュリティ研究会（ICSS）
+        <details className="achievement_details">
+          <summary>研究テーマ</summary>
+          <ul>
+            <li>
+              <a href="https://www.ieice.org/iss/icss/award.html">
+                <img src="https://img.shields.io/badge/ICSS研究賞-2020-E3C96F" alt="ICSS badge" />
+              </a>
+              「仮想背景を使用したリモート会議映像における秘匿された背景の再構築手法」
+              ○辻知希，石川琉聖（立命館大）・衛藤将史（NICT）・服部祐一（セキュアサイクル）・井上博之（広島市大）
+            </li>
+            <li>
+              「プログラミングコンテストにおけるソースコードの盗作検知手法の実装と評価」
+              ○石川琉聖（立命館大）・服部祐一（セキュアサイクル）・井上博之（広島市大）・猪俣敦夫（阪大）
+            </li>
+          </ul>
+        </details>
+      </>
+    ),
+    url: 'https://www.ieice.org/ken/program/index.php?tgs_regid=4674e49d7365cbd99b30867d8c415e9417ba71c10dcd35acf8e0ca9a9d813f1b&tgid=IEICE-ICSS'
+  },
+  {
+    year: '2021',
+    detail: 'ICPC アジア地区横浜大会',
+    url: 'https://icpc.iisf.or.jp/2020-yokohama/domestic'
+  }
+]
+
+const MediaData = [
+  {
+    year: '2021',
+    detail: 'サイバーセキュリティⅡ 第２回 情報セキュリティ教育と人材育成 BS231ch',
+    url: 'https://www.ouj.ac.jp/hp/o_itiran/2021/0205.html'
+  }
+]
+
+const HackData = [
+  {
+    year: '2020-',
+    detail: (
+      <>
+        IPA 脆弱性関連情報届出受理 <IPAVulnList type="num" />件
+        <details className="achievement_details">
+          <summary>取得番号一覧</summary>
+          <div>
+            <IPAVulnList type="list" />
+          </div>
+        </details>
+      </>
+    ),
+    url: 'https://www.ipa.go.jp/security/vuln/report'
+  }
+]
+
+const QualificationData = [
+  {
+    year: '2017',
+    detail: 'ITパスポート',
+    url: 'https://www3.jitec.ipa.go.jp/JitesCbt/index.html'
+  },
+  {
+    year: '2017',
+    detail: '実用数学技能検定 2級',
+    url: 'https://www.su-gaku.net/suken/'
+  },
+  {
+    year: '2018',
+    detail: '応用情報技術者',
+    url: 'https://www.jitec.ipa.go.jp/1_11seido/ap.html'
+  },
+  {
+    year: '2020',
+    detail: 'TOEIC 610点',
+    url: 'https://www.iibc-global.org/toeic.html'
+  },
+  {
+    year: '2021',
+    detail: '2級鍵師技能検定',
+    url: 'https://www.kagishi.com/examination/kagishi2'
+  }
+]
+
+const AchievementData = [
+  { title: 'School', Content: SchoolData },
+  { title: 'Work', Content: WorkData },
+  { title: 'Event', Content: EventData },
+  { title: 'Media', Content: MediaData },
+  { title: 'Hack', Content: HackData },
+  { title: 'Qualification', Content: QualificationData }
+]
+
+export default AchievementData
