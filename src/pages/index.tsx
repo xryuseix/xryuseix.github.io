48dec2d86dcfef6869b5e11118eed774f413a30c
xryuseix <ryusei143.shootingstar@gmail.com>
2021-12-06 02:36:24 +0900

    refactor: index.jsのリファクタリング

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,59 +17,70 @@ const faviconStyle: React.CSSProperties = {
   borderRadius: '50%'
 }
 
+const Profile: React.VFC = () => {
+  return (
+    <div className="index_flexbox">
+      <div className="index_icon">
+        <StaticImage src="../../static/favicons/icon-256x256.png" alt="icon" style={faviconStyle} />
+      </div>
+      <section style={{ padding: '20px' }}>
+        <div className="index_rotate">
+          <h3 style={{ margin: '0px' }}>Programmer</h3>
+          <span className="index_front">xryuseix</span>
+          <span className="index_back">Ryusei&nbsp;Ishikawa</span>
+        </div>
+        <div className="index_box">
+          <span className="index_box-title">Study</span>
+          Algorithm &amp; Security
+        </div>
+        <div className="index_box">
+          <span className="index_box-title">College</span>
+          Ritsumeikan University
+        </div>
+      </section>
+    </div>
+  )
+}
+
+const BottomButtons: React.VFC = () => {
+  return (
+    <ul className="index_footer-menu">
+      <li className="index_footer-column index_scale">
+        <Link to="/apps">
+          <h3>Apps</h3>
+        </Link>
+      </li>
+      <li className="index_footer-column index_scale">
+        <Link to="/achievement">
+          <h3>Achieve</h3>
+        </Link>
+      </li>
+      <li className="index_footer-column index_scale">
+        <Link to="/account">
+          <h3>Account</h3>
+        </Link>
+      </li>
+      <li className="index_footer-column index_scale">
+        <Link to="/ctf">
+          <h3>CTF</h3>
+        </Link>
+      </li>
+      <li className="index_footer-column index_scale">
+        <Link to="/lt">
+          <h3>Slides</h3>
+        </Link>
+      </li>
+    </ul>
+  )
+}
+
 const SiteIndex: React.VFC<IndexPageProps> = (Props: IndexPageProps) => {
   return (
     <Layout location={Props.location}>
       <SEO title="xryuseix" description="ここがトップページや！！！！！！！！！" />
       <Meta title="Home" />
-      <div className="index_flexbox">
-        <div className="index_icon">
-          <StaticImage src="../../static/favicons/icon-256x256.png" alt="icon" style={faviconStyle} />
-        </div>
-        <section style={{ padding: '20px' }}>
-          <div className="index_rotate">
-            <h3 style={{ margin: '0px' }}>Programmer</h3>
-            <span className="index_front">xryuseix</span>
-            <span className="index_back">Ryusei&nbsp;Ishikawa</span>
-          </div>
-          <div className="index_box">
-            <span className="index_box-title">Study</span>
-            Algorithm &amp; Security
-          </div>
-          <div className="index_box">
-            <span className="index_box-title">College</span>
-            Ritsumeikan University
-          </div>
-        </section>
-      </div>
-
-      <ul className="index_footer-menu">
-        <li className="index_footer-column index_scale">
-          <Link to="/apps">
-            <h3>Apps</h3>
-          </Link>
-        </li>
-        <li className="index_footer-column index_scale">
-          <Link to="/achievement">
-            <h3>Achieve</h3>
-          </Link>
-        </li>
-        <li className="index_footer-column index_scale">
-          <Link to="/account">
-            <h3>Account</h3>
-          </Link>
-        </li>
-        <li className="index_footer-column index_scale">
-          <Link to="/ctf">
-            <h3>CTF</h3>
-          </Link>
-        </li>
-        <li className="index_footer-column index_scale">
-          <Link to="/lt">
-            <h3>Slides</h3>
-          </Link>
-        </li>
-      </ul>
+      <Profile />
+      <BottomButtons />
     </Layout>
   )
 }
