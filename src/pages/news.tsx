745c7c7a1d51c46f082435f113ce32ec3d543bcb
xryuseix <ryusei143.shootingstar@gmail.com>
2021-12-09 03:08:56 +0900

    refactor: news.jsをTSにした

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
+import { Link, graphql, PageProps } from 'gatsby'
 
 import Meta from '../components/meta'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
-const BlogIndex = ({ data, location }) => {
+const BlogPageIndex: React.FC<PageProps<GatsbyTypes.BlogIndexQuery>> = ({ data, location }) => {
   const posts = data.allMarkdownRemark.nodes
   return (
     <Layout location={location}>
@@ -13,25 +13,25 @@ const BlogIndex = ({ data, location }) => {
       <Meta title="news" />
       <h1>News</h1>
       <p>このページの新機能やブログの投稿，ちょっとしたニュースをここに記載します</p>
-      <ol style={{ listStyle: `none` }}>
+      <ol style={{ listStyle: 'none' }}>
         {posts.map((post) => {
-          const title = post.frontmatter.title || post.fields.slug
+          const title = post.frontmatter?.title || post.fields?.slug
 
           return (
-            <li key={post.fields.slug}>
+            <li key={post.fields?.slug}>
               <article className="post-list-item" itemScope itemType="http://schema.org/Article">
                 <header>
                   <h2>
-                    <Link to={post.fields.slug} itemProp="url">
+                    <Link to={post.fields?.slug || '/'} itemProp="url">
                       <span itemProp="headline">{title}</span>
                     </Link>
                   </h2>
-                  <small>{post.frontmatter.date}</small>
+                  <small>{post.frontmatter?.date}</small>
                 </header>
                 <section>
                   <p
                     dangerouslySetInnerHTML={{
-                      __html: post.frontmatter.description || post.excerpt
+                      __html: post.frontmatter?.description || post.excerpt || ''
                     }}
                     itemProp="description"
                   />
@@ -46,7 +46,7 @@ const BlogIndex = ({ data, location }) => {
   )
 }
 
-export default BlogIndex
+export default BlogPageIndex
 
 export const pageQuery = graphql`
   query BlogIndex {
