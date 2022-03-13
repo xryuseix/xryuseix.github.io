2adf067bdea813b49f40ec053d7f7ae02851fb04
xryuseix <ryusei143.shootingstar@gmail.com>
2022-03-13 13:20:43 +0900

    feat: OGP画像を設定

--- components/Layout/Head.tsx
+++ components/Layout/Head.tsx
@@ -5,27 +5,28 @@ import Head from "next/head";
 interface Props {
   title?: string;
   description?: string;
+  ogImage?: string;
 }
 
-const Header: React.FC<Props> = ({ title, description }: Props) => {
-  const defaultDescription =
-    "xryuseixのWebページです.競技プログラミングやセキュリティの分野を勉強しています.";
+const Header: React.FC<Props> = ({ title, description, ogImage }: Props) => {
+  const defaultDescription = "xryuseixのWebページです.";
   const defaultTitle = "xryuseix's HomePage";
+  const defaultOgImage = "https://avatars.githubusercontent.com/u/51394682";
+  const pageTitle = title || defaultTitle;
+  const pageDescription = description || defaultDescription;
+  const OGPImage = ogImage || defaultOgImage;
   return (
     <Head>
-      <title>{title ?? defaultTitle}</title>
+      <title>{pageTitle}</title>
       <meta charSet="UTF-8" />
       <meta httpEquiv="content-language" content="ja" />
-      <meta name="description" content={description ?? defaultDescription} />
+      <meta name="description" content={pageDescription} />
       <link rel="icon" href="/favicon.ico" />
-      <meta name="og:title" content={title} />
+      <meta name="og:title" content={pageTitle} />
+      <meta property="og:description" content={pageDescription} />
       <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary_large_image" />
-      {/* TODO: 後ほど作成する */}
-      {/* <meta
-        property="og:image"
-        content="https://avatars.githubusercontent.com/u/7278547?s=200&amp;v=4"
-      /> */}
+      <meta name="twitter:card" content="summary" />
+      <meta property="og:image" content={OGPImage} />
     </Head>
   );
 };
